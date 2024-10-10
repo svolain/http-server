44a2ca52952acd98fed779f7e155687ec1cd7883
Dmitrii Shatilov <shatilovdr@gmail.com>
2024-10-10 16:12:12 +0300

    split ParseHeader into several functions,move some functions to private part

--- includes/HttpParser.hpp
+++ includes/HttpParser.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HttpParser.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsavolai <vsavolai@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 12:16:12 by vsavolai          #+#    #+#             */
-/*   Updated: 2024/10/10 13:16:13 by vsavolai         ###   ########.fr       */
+/*   Updated: 2024/10/10 16:05:40 by dshatilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,13 @@ class VirtualHost;
 class HttpParser {
  public:
   HttpParser() = default;
-  HttpParser(const std::string buffer);
+
   ~HttpParser() = default;
 
   bool        ParseHeader(const std::string& buffer);
   int         WriteBody(VirtualHost* vhost,  std::vector<char>& buffer,
                         int bytesIn);
-  bool        UnChunkBody(std::vector<char>& buf);
-  void        AppendBody(std::vector<char> buffer, int bytesIn);
   bool        IsBodySizeValid(VirtualHost* vhost) ;
-  void        HandlePostRequest(std::vector<char> request_body, HttpParser &parser);
-  bool        HandleMultipartFormData(const std::vector<char> &body, const std::string &contentType);
-  bool        ParseMultiPartData(std::vector<char> &bodyPart);
-  bool        ParseUrlEncodedData(const std::vector<char>& body);
   std::string getHost() const;
   std::string getMethod() const;
   std::string getResourcePath() const;
@@ -52,15 +46,25 @@ class HttpParser {
   void        setErrorCode(int error) ;
 
  private:
+  bool  ParseStartLine(std::istringstream& request_stream);
+  bool  ParseHeaderFields(std::istringstream& request_stream);
+  bool  CheckPostHeaders();
+  bool  UnChunkBody(std::vector<char>& buf);
+  void  AppendBody(std::vector<char> buffer, int bytesIn);
+  void  HandlePostRequest(std::vector<char> request_body, HttpParser& parser);
+  bool  HandleMultipartFormData(const std::vector<char>& body,
+                                const std::string& contentType);
+  bool  ParseMultiPartData(std::vector<char>& bodyPart);
+  bool  ParseUrlEncodedData(const std::vector<char>& body);
+
   int                                 error_code_ = 200;
   size_t                              content_length_ = 0;
   std::string                         method_;
-  std::string                         resource_path_;
+  std::string                         request_target_;
   std::string                         query_string_;
-  std::string                         http_version_;
   std::vector<char>                   request_body_;
   std::map<std::string, std::string>  headers_ = {};
-  bool                                is_chunked_;
+  bool                                is_chunked_ = false;
 };
 
 #endif
--- srcs/Webserv/HttpParser.cpp
+++ srcs/Webserv/HttpParser.cpp
@@ -3,113 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   HttpParser.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vsavolai <vsavolai@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: dshatilo <dshatilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 13:13:54 by vsavolai          #+#    #+#             */
-/*   Updated: 2024/10/10 13:23:31 by vsavolai         ###   ########.fr       */
+/*   Updated: 2024/10/10 16:08:52 by dshatilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HttpParser.hpp"
 #include "Logger.h"
 
-HttpParser::HttpParser(const std::string request) //We don't use it yet
-    : error_code_(0), is_chunked_(false) {
-  ParseHeader(request);
-}
-
 bool HttpParser::ParseHeader(const std::string& request) {
   std::istringstream  request_stream(request);
-  std::string         line;
 
-  request_stream >> method_ >> resource_path_ >> http_version_;
-  std::getline(request_stream, line);
-   //what if http_version is incorrect?
-  if (method_.empty() || resource_path_.empty()
-      || http_version_.empty() || line != "\r") {
-    logError("Error: bad request 400");
-    //respond with HTTP 400 Bad Request
-    error_code_ = 400;
+  if (!ParseStartLine(request_stream))
     return false;
-  }
-
-  static std::array<std::string, 4> allowed_methods = {
-      "GET", "POST", "DELETE", "HEAD"};
-  if (std::find(allowed_methods.begin(), allowed_methods.end(), method_) ==
-      allowed_methods.end()) {
-    logError("Error: not supported method requested");
-    //respond with HTTP 501 Not Implemented Error
-    error_code_ = 501;
+  if (!ParseHeaderFields(request_stream))
     return false;
-  }
-
-  size_t query_position = resource_path_.find("?");
-  if (query_position != std::string::npos) {
-    query_string_ = resource_path_.substr(query_position + 1);
-    resource_path_ = resource_path_.substr(0, query_position);
-  }
-
-                        // if (!CheckValidPath(resource_path_)) {      //Path directories'll be checked later
-                        //   return false;
-                        // }
-
-  while (std::getline(request_stream, line) && line != "\r") {
-    size_t delim = line.find(":");
-    if (delim == std::string::npos || line.back() != '\r') {
-      logError("Error: wrong header line format");
-      //respond with http 400 Bad Request
-      error_code_ = 400;
-      return false; 
-    }
-    line.pop_back();
-    std::string header = line.substr(0, delim);
-    std::string header_value = line.substr(delim + 1);
-    header_value.erase(0, header_value.find_first_not_of(" \t"));
-    headers_[header] = header_value;
-    //Ideally for specific headers server should check for duplicates and send 400 if it finds them (two hosts or content-length for ex)
-  }
-  if (!headers_.contains("Host")) {
-    logError("Error: bad request 400");
-    error_code_ = 400;
+  if (method_ == "POST" && !CheckPostHeaders())
     return false;
-  }
-  if (line != "\r") {
-    logError("Error: Request Header Fields Too Large");
-    error_code_ = 431;
-    return false;
-  }
-
-  if (method_ == "GET" || method_ == "HEAD")
+  else if (method_ == "DELETE") //CHECKS FOR DELETE METHOD IF NEEDED                  
+    ;
+  else
     return true;
-
-  auto it = headers_.find("transfer-encoding");
-  is_chunked_ = (it != headers_.end() && it->second == "chunked");
-
-  if (!is_chunked_) {
-    auto it = headers_.find("Content-Length");
-    if (it == headers_.end()) {
-      logError("Error: content-lenght missing for request body");
-      //respond with http 411 Length Required
-      error_code_ = 411;
-      return false;
-    } else {
-      std::string& content_length_str = it->second;
-      try {
-        size_t pos;
-        content_length_ = std::stoi(content_length_str, &pos);
-        if (pos != content_length_str.size() || content_length_ < 0)
-          throw std::invalid_argument("Invalid argument");
-      } catch (const std::invalid_argument& e) {
-        logError("Error: invalid Content-Length");
-        error_code_ = 400; // Bad Request for invalid Content-Length
-        return false;
-      } catch (const std::out_of_range& e) {
-        logError("Error: Content-Length out of range");
-        error_code_ = 413; // Bad Request for excessively large Content-Length
-        return false;
-      }
-    }
-  }
   std::streampos current = request_stream.tellg();
   request_stream.seekg(0, std::ios::end); /* Beware of off by 1, should be good tho */
   size_t stream_size = request_stream.tellg() - current;
@@ -135,6 +51,130 @@ int HttpParser::WriteBody(VirtualHost* vhost, std::vector<char>& buffer,
   }
   UnChunkBody(request_body_);
   return 1;
+}
+
+bool  HttpParser::IsBodySizeValid(VirtualHost* vhost) {
+  if (request_body_.size() > vhost->getMaxBodySize()) {
+    logError("Error: Request Header Fields Too Large");
+    error_code_ = 431;
+    return false;
+  }
+  return true;
+}
+
+std::string  HttpParser::getHost() const {
+  return headers_.at("Host");
+}
+
+std::string HttpParser::getMethod() const {
+  return method_;
+}
+
+std::string HttpParser::getResourcePath() const {
+  return request_target_;
+}
+
+int HttpParser::getErrorCode() const {
+  return error_code_;
+}
+
+void  HttpParser::setErrorCode(int error) {
+  error_code_ = error;
+}
+
+bool  HttpParser::ParseStartLine(std::istringstream& request_stream) {
+  std::string line;
+  std::string http_version;
+  std::getline(request_stream, line);
+  std::istringstream line_stream(line);
+  line_stream >> method_ >> request_target_ >> http_version;
+  std::getline(line_stream, line);
+
+  if (method_.empty() || request_target_.empty()
+      || http_version.empty() || line != "\r") {
+    logError("Error: bad request 400");
+    error_code_ = 400;
+    return false;
+  }
+
+  static std::array<std::string, 4> allowed_methods = {
+      "GET", "POST", "DELETE", "HEAD"};
+  if (std::find(allowed_methods.begin(), allowed_methods.end(), method_) ==
+      allowed_methods.end()) {
+    logError("Error: not supported method requested");
+    error_code_ = 501;
+    return false;
+  }
+
+  if (request_target_[0] != '/' || http_version != "HTTP/1.1") {
+    logError("Error: bad request 400");
+    error_code_ = 400;
+    return false;
+  }
+
+  size_t query_position = request_target_.find("?");
+  if (query_position != std::string::npos) {
+    query_string_ = request_target_.substr(query_position + 1);
+    request_target_ = request_target_.substr(0, query_position);
+  }
+  return true;
+}
+
+bool  HttpParser::ParseHeaderFields(std::istringstream& request_stream) {
+  std::string line;
+  while (std::getline(request_stream, line) && line != "\r") {
+    size_t delim = line.find(":");
+    if (delim == std::string::npos || line.back() != '\r') {
+      logError("Error: wrong header line format");
+      error_code_ = 400;
+      return false; 
+    }
+    line.pop_back();
+    std::string header = line.substr(0, delim);
+    std::string header_value = line.substr(delim + 1);
+    headers_[header] = header_value;
+  }
+  if (!headers_.contains("Host")) {
+    logError("Error: bad request 400");
+    error_code_ = 400;
+    return false;
+  }
+  if (line != "\r") {
+    logError("Error: Request Header Fields Too Large");
+    error_code_ = 431;
+    return false;
+  }
+  return true;
+}
+
+bool  HttpParser::CheckPostHeaders() {
+  auto it = headers_.find("transfer-encoding");
+  is_chunked_ = (it != headers_.end() && it->second == "chunked");
+  if (!is_chunked_) {
+    auto it = headers_.find("Content-Length");
+    if (it == headers_.end()) {
+      logError("Error: content-lenght missing for request body");
+      error_code_ = 411;
+      return false;
+    } else {
+      std::string& content_length_str = it->second;
+      try {
+        size_t pos;
+        content_length_ = std::stoi(content_length_str, &pos);
+        if (pos != content_length_str.size() || content_length_ < 0)
+          throw std::invalid_argument("Invalid argument");
+      } catch (const std::invalid_argument& e) {
+        logError("Error: invalid Content-Length");
+        error_code_ = 400;
+        return false;
+      } catch (const std::out_of_range& e) {
+        logError("Error: Content-Length out of range");
+        error_code_ = 413;
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 bool HttpParser::UnChunkBody(std::vector<char>& buf) {
@@ -190,18 +230,61 @@ bool HttpParser::UnChunkBody(std::vector<char>& buf) {
   return true;
 }
 
-void HttpParser::AppendBody(std::vector<char> buffer, int bytesIn) {
-  request_body_.insert(request_body_.end(), buffer.begin(),
-                       buffer.begin() + bytesIn);
+void  HttpParser::HandlePostRequest(std::vector<char> request_body, HttpParser &parser) {
+    std::string contentType = headers_.at("Content-Type");
+    parser.setErrorCode(200);
+
+  if (contentType.find("application/x-www-form-urlencoded") != std::string::npos) {
+        std::cout << "Handling URL-encoded form submission" << std::endl;
+        if (!ParseUrlEncodedData(request_body)) {
+             parser.setErrorCode(500); // Internal Server Error
+            return;
+        }
+        
+    } else if (contentType.find("multipart/form-data") != std::string::npos) {
+        if (!HandleMultipartFormData(request_body, contentType)) {
+            parser.setErrorCode(500); // Internal Server Error
+            return;
+        }
+    } else {
+        std::cerr << "Unsupported Content-Type: " << contentType << std::endl;
+        parser.setErrorCode(415);
+    }
 }
 
-bool  HttpParser::IsBodySizeValid(VirtualHost* vhost) {
-  if (request_body_.size() > vhost->getMaxBodySize()) {
-    logError("Error: Request Header Fields Too Large");
-    error_code_ = 431;
-    return false;
-  }
-  return true;
+bool HttpParser::HandleMultipartFormData(const std::vector<char> &body, const std::string &contentType) {
+    std::cout << "Multipart form data received: " << body.size() << " bytes" << std::endl;
+
+    size_t boundaryPosition = contentType.find("boundary=");
+    if (boundaryPosition == std::string::npos)
+        return false;
+
+    std::string boundary = "--" + contentType.substr(boundaryPosition + 9);
+    std::cout << "boundary:\n" << boundary << "\n";
+
+    std::vector<char> boundaryVec(boundary.begin(), boundary.end());
+    size_t boundaryLength = boundaryVec.size();
+
+    auto pos = body.begin();
+
+    while (true) {
+        auto boundaryStart = std::search(pos, body.end(), boundaryVec.begin(), boundaryVec.end());
+        if (boundaryStart == body.end())
+            break;
+
+        pos = boundaryStart + boundaryLength;
+
+        auto nextBoundaryStart = std::search(pos, body.end(), boundaryVec.begin(), boundaryVec.end());
+
+        std::vector<char> bodyPart(pos, nextBoundaryStart);
+
+        if (!ParseMultiPartData(bodyPart))
+            return false;
+
+        pos = nextBoundaryStart;
+    }
+
+    return true;
 }
 
 bool HttpParser::ParseUrlEncodedData(const std::vector<char>& body) {
@@ -288,81 +371,9 @@ bool HttpParser::ParseMultiPartData(std::vector<char> &bodyPart) {
     return true;
 }
 
-bool HttpParser::HandleMultipartFormData(const std::vector<char> &body, const std::string &contentType) {
-    std::cout << "Multipart form data received: " << body.size() << " bytes" << std::endl;
-
-    size_t boundaryPosition = contentType.find("boundary=");
-    if (boundaryPosition == std::string::npos)
-        return false;
-
-    std::string boundary = "--" + contentType.substr(boundaryPosition + 9);
-    std::cout << "boundary:\n" << boundary << "\n";
-
-    std::vector<char> boundaryVec(boundary.begin(), boundary.end());
-    size_t boundaryLength = boundaryVec.size();
-
-    auto pos = body.begin();
-
-    while (true) {
-        auto boundaryStart = std::search(pos, body.end(), boundaryVec.begin(), boundaryVec.end());
-        if (boundaryStart == body.end())
-            break;
-
-        pos = boundaryStart + boundaryLength;
-
-        auto nextBoundaryStart = std::search(pos, body.end(), boundaryVec.begin(), boundaryVec.end());
-
-        std::vector<char> bodyPart(pos, nextBoundaryStart);
-
-        if (!ParseMultiPartData(bodyPart))
-            return false;
-
-        pos = nextBoundaryStart;
-    }
-
-    return true;
-}
-
-void  HttpParser::HandlePostRequest(std::vector<char> request_body, HttpParser &parser) {
-    std::string contentType = headers_.at("Content-Type");
-    parser.setErrorCode(200);
-
-  if (contentType.find("application/x-www-form-urlencoded") != std::string::npos) {
-        std::cout << "Handling URL-encoded form submission" << std::endl;
-        if (!ParseUrlEncodedData(request_body)) {
-             parser.setErrorCode(500); // Internal Server Error
-            return;
-        }
-        
-    } else if (contentType.find("multipart/form-data") != std::string::npos) {
-        if (!HandleMultipartFormData(request_body, contentType)) {
-            parser.setErrorCode(500); // Internal Server Error
-            return;
-        }
-    } else {
-        std::cerr << "Unsupported Content-Type: " << contentType << std::endl;
-        parser.setErrorCode(415);
-    }
-}
-
-std::string  HttpParser::getHost() const {
-  return headers_.at("Host");
-}
-
-std::string HttpParser::getMethod() const {
-  return method_;
-}
-
-std::string HttpParser::getResourcePath() const {
-  return resource_path_;
-}
-
-int HttpParser::getErrorCode() const {
-  return error_code_;
-}
-
-void  HttpParser::setErrorCode(int error) {
-  error_code_ = error;
+void HttpParser::AppendBody(std::vector<char> buffer, int bytesIn) {
+  request_body_.insert(request_body_.end(), buffer.begin(),
+                       buffer.begin() + bytesIn);
 }
 
 // bool HttpParser::CheckValidPath(std::string path) {
